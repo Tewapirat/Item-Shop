6571eb6d3e27c6fb6c3f874cc5bc67815e95281e
ApiratK <>
2025-12-05 18:52:12 +0700

    :package: add model itemManaging

---
+++ pkg/itemManaging/model/itemManaging.go
@@ -0,0 +1,20 @@
+package model
+
+type (
+	ItemCreatingReq struct {
+		AdminID		string
+		Name 		string `json:"name" validate:"required,max=64"`
+		Description string `json:"description" validate:"required,max=128"`
+		Picture		string `json:"picture" validate:"required"`
+		Price		uint `json:"price" validate:"required"`
+	}
+
+	ItemEditingReq struct {
+		AdminID		string
+		Name 		string `json:"name" validate:"required,max=64"`
+		Description string `json:"description" validate:"required,max=128"`
+		Picture		string `json:"picture" validate:"omitempty"`
+		Price		uint `json:"price" validate:"omitempty"`
+
+	}
+)
