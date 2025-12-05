a85897da5aeb31e5a78e8e703d7b67f34c899143
ApiratK <>
2025-12-05 18:50:40 +0700

    :package: add itemManagingService

---
+++ pkg/itemManaging/service/itemManagingService.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	_itemManagingModel "github.com/TewApirat/items-shop-api/pkg/itemManaging/model"
+	_itemShopModel "github.com/TewApirat/items-shop-api/pkg/itemShop/model"
+)
+
+type ItemManagingService interface {
+	Creating(ItemCreatingReq *_itemManagingModel.ItemCreatingReq) ( *_itemShopModel.Item,error)
+}
