b139fd62712e911a48db53371b8bc0e53ccfba53
ApiratK <>
2025-12-05 18:49:56 +0700

    :package: add itemManagingControllerImpl

---
+++ pkg/itemManaging/controller/itemManagingControllerImpl.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/TewApirat/items-shop-api/pkg/custom"
+	_itemManagingModel "github.com/TewApirat/items-shop-api/pkg/itemManaging/model"
+	_itemManagingService "github.com/TewApirat/items-shop-api/pkg/itemManaging/service"
+	"github.com/labstack/echo/v4"
+)
+
+type itemManagingControllerImpl struct {
+
+itemManagingService _itemManagingService.ItemManagingService
+
+}
+
+func NewItemManagingControllerImpl(itemManagingService _itemManagingService.ItemManagingService) ItemManagingController{
+	return &itemManagingControllerImpl{itemManagingService}
+}
+
+func (c * itemManagingControllerImpl)Creating(pctx echo.Context) error{
+	itemCreatingReq := new(_itemManagingModel.ItemCreatingReq)
+
+	customeEchoRequest := custom.NewCustomEchoRequest(pctx)
+
+	if err := customeEchoRequest.Bind(itemCreatingReq); err != nil{
+	return custom.Error(pctx, http.StatusBadRequest, err.Error())
+	}
+
+	item, err := c.itemManagingService.Creating(itemCreatingReq)
+	if err != nil{
+		return custom.Error(pctx,http.StatusInternalServerError, err.Error())
+	}
+	return pctx.JSON(http.StatusCreated, item)
+
+}
